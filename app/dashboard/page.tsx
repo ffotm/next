2e456631eb66b8a96a98ca6d8f76e0fe7ff1ceb1
unknown <otmani.fad@gmail.com>
2025-08-12 18:55:57 +0100

    I

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
+
 export default function Page() {
     return <p>Dashboard Page</p>;
-=======
-export default function Page() {
-    return <p>Dashboard Page</p>;
->>>>>>> 78bd056aa552d416095a02b4d53316f116f0a8c1
+
 }
