4d9df2a7d7c591e7ac4f1187a54864ea48df37f4
unknown <otmani.fad@gmail.com>
2025-08-12 18:52:33 +0100

    Initial local commit

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { Inter, Lusitana } from 'next/font/google';
 
 
@@ -7,14 +7,5 @@ export const lusitana = Lusitana({
     subsets: ['latin'],
     weight: ['400', '700'],
     variable: '--font-lusitana',
-=======
-import { Inter, Lusitana } from 'next/font/google';
 
-
-export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({
-    subsets: ['latin'],
-    weight: ['400', '700'],
-    variable: '--font-lusitana',
->>>>>>> 78bd056aa552d416095a02b4d53316f116f0a8c1
 });
